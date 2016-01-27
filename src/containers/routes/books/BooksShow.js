45afd4bf2d7bd36e6d3d0610cd77e56890b04e72
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2016-01-27 15:43:04 +0100

    Implemented show action

---
+++ src/containers/routes/books/BooksShow.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Router, Route, Link } from 'react-router';
+
+import FlatButton from 'components/FlatButton';
+
+import Styles from 'stylesheets/containers/routes/books/BooksShow.sass';
+
+import * as Actions from 'actions/Actions';
+
+/**
+ * Layout
+ */
+export class BooksShow extends React.Component {
+  render() {
+    const book = _.findWhere(this.props.books, { isbn: this.props.params.id });
+
+    return (
+      <div className={Styles.Books}>
+        <h2>
+          Book: {book.title}
+        </h2>
+        <p>
+          {book.subtitle}
+        </p>
+        <p>
+          {book.abstract}
+        </p>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  (state) => ({
+    books: state.books.collection
+  }),
+  (dispatch) => ({
+    actions: bindActionCreators(Actions, dispatch)
+  })
+)(BooksShow);
